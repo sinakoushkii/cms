8bf526928ed18e1460a3a64f226bdbc556f5fb49
Sina <sinaderik7@gmail.com>
2024-11-25 11:12:50 +0330

    redux store is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from '@mui/material'
+import { Container } from '@mui/material'
 import React from 'react'
 
 const App = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import store from "./store.js";
+import { Provider } from "react-redux";
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
