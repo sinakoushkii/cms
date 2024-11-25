7fd2bc026fdf3a1d7e59c4f7ae3bee75769aceae
Sina <sinaderik7@gmail.com>
2024-11-25 11:40:14 +0330

    redux is completely configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Container } from '@mui/material'
+import { Button, Container } from '@mui/material'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './features/post/postSlice';
 
 const App = () => {
+  const count =useSelector(state=>state.posts.value);
+  const dispatch = useDispatch();
+
   return (
     <Container fixed>
-        Test
+        <Button onClick={()=>dispatch(increment())} variant="contained">increment</Button>
+          <span>Count: {count}</span>
+        <Button onClick={()=>dispatch(decrement())} variant="contained">decrement</Button>
     </Container>
   )
 }
--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,24 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const postSlice = createSlice({
+  name: 'posts',
   initialState: {
-    value: 0
+    value: []
   },
   reducers: {
-    increment: state => {
-      state.value += 1
-    },
-    decrement: state => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    }
+
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+// export const { increment, decrement, incrementByAmount } = postSlice.actions
 
-export default counterSlice.reducer
+export default postSlice.reducer
