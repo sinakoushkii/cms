63b5b7dd465ef1c4cae53df660e349945cfc89b8
Sina <sinaderik7@gmail.com>
2024-11-25 11:48:49 +0330

    useless line is removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { Button, Container } from '@mui/material'
+import { Container } from '@mui/material'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment } from './features/post/postSlice';
 
 const App = () => {
-  const count =useSelector(state=>state.posts.value);
-  const dispatch = useDispatch();
-
   return (
     <Container fixed>
-        <Button onClick={()=>dispatch(increment())} variant="contained">increment</Button>
-          <span>Count: {count}</span>
-        <Button onClick={()=>dispatch(decrement())} variant="contained">decrement</Button>
+        
     </Container>
   )
 }
